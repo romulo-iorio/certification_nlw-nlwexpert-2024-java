709f4732e1edfc08c13333abe131fdce4a759593
Rômulo Iorio <romuloiorio@hotmail.com>
2024-02-10 22:30:20 -0300

    feat: creating entities

---
+++ src/main/java/com/example/certification_nlw/modules/students/entities/CertificationStudentEntity.java
@@ -0,0 +1,19 @@
+package com.example.certification_nlw.modules.students.entities;
+
+import java.util.List;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CertificationStudentEntity {
+    private UUID id;
+    private UUID studentId;
+    private String technology;
+    private int grade;
+    List<AnswersCertificationEntity> answers;
+}
